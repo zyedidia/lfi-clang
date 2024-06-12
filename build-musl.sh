a07a62b189cca91b8f17a979ecb8e1f69cc7f1f3
Zachary Yedidia <zyedidia@gmail.com>
2024-06-11 22:15:45 -0700

    amd64 support

--- build-musl.sh
+++ build-musl.sh
@@ -12,9 +12,10 @@ COMPRT=$PREFIX/compiler-rt
 
 CC=$1
 
-cp musl-custom/memset.S musl-$VERSION/src/string/aarch64/memset.S
 cp musl-custom/getopt.c musl-$VERSION/src/misc/getopt.c
-cp musl-custom/crti.s musl-$VERSION/crt/aarch64/crti.s
+cp musl-custom/aarch64/memset.S musl-$VERSION/src/string/aarch64/memset.S
+cp musl-custom/aarch64/crti.s musl-$VERSION/crt/aarch64/crti.s
+cp musl-custom/x86_64/crti.s musl-$VERSION/crt/x86_64/crti.s
 
 export CC=$CC
 export CFLAGS="--rtlib=compiler-rt -resource-dir $COMPRT"
---
+++ musl-custom/x86_64/crti.s
@@ -0,0 +1,11 @@
+.section .init
+.global _init
+_init:
+	push %rax
+	.p2align 4
+
+.section .fini
+.global _fini
+_fini:
+	push %rax
+	.p2align 4
